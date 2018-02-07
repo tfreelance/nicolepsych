19e7e7fd3b6e933ad3ca612e6527c0b488f087b2
Nicol <nicolehartpractice@gmail.com>
2018-02-07 14:17:11 -0800

    Updating from create-react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <div className="App-header">
+          <h1 className="App-title">Nicole Nemiroff Psychotherapy</h1>
+          <h3 className="App-intro">This site is currently under construction. Please come back soon.</h3>
+          <h3 className="App-intro App-lower">In the meantime, feel free to contact me at:</h3>
+        </div>
+        <div>
+        <p className="App-contact">
+          <a href="mailto:nicole.nemiroff@gmail.com">
+              nicole.nemiroff@gmail.com
+            </a> 
+
+            &nbsp;&nbsp;&nbsp; 
+
+            <a href="tel:14155859934">+1(415) 484-9934</a>
         </p>
+        <p className="App-contact App-lower">
+            <a href="https://www.google.com/maps/place/219,+3150+18th+St,+San+Francisco,+CA+94110/@37.7622588,-122.4185229,
+                  17z/data=!3m1!4b1!4m5!3m4!1s0x808f7e3af2e063d5:0xe2c3e00a9954dda2!8m2!3d37.7622588!4d-122.4141455">
+              3150 18th St. San Francisco, CA 
+            </a>
+        </p>
+        </div>
       </div>
     );
   }
