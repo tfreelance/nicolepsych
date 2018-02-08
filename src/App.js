3c83df70ab3be9da9a58603a1960f62fcdfbc54f
Nicol <nicolehartpractice@gmail.com>
2018-02-08 10:34:57 -0800

    updating contact section

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ class App extends Component {
         </div>
         <div>
         <p className="App-contact">
+          <a href="tel:14155859934">+1(415) 484-9934</a>
+        </p>
+        <p className="App-contact App-lower">
           <a href="mailto:nicole.nemiroff@gmail.com">
               nicole.nemiroff@gmail.com
             </a> 
-
-            &nbsp;&nbsp;&nbsp; 
-
-            <a href="tel:14155859934">+1(415) 484-9934</a>
         </p>
         <p className="App-contact App-lower">
             <a href="https://www.google.com/maps/place/219,+3150+18th+St,+San+Francisco,+CA+94110/@37.7622588,-122.4185229,
-                  17z/data=!3m1!4b1!4m5!3m4!1s0x808f7e3af2e063d5:0xe2c3e00a9954dda2!8m2!3d37.7622588!4d-122.4141455">
+                  17z/data=!3m1!4b1!4m5!3m4!1s0x808f7e3af2e063d5:0xe2c3e00a9954dda2!8m2!3d37.7622588!4d-122.4141455"
+                  target="_blank">
               3150 18th St. San Francisco, CA 
             </a>
         </p>
